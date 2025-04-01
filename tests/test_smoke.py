bfd3ec2e5874c9a0781e3af6c8767ea95963abb9
Dragomir Penev <dragomir.penev@canonical.com>
2025-04-01 16:50:57 +0300

    Add unit tests and ruff linting

---
+++ tests/test_smoke.py
@@ -0,0 +1,69 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+import subprocess
+
+import yaml
+
+
+def test_upload():
+    with open("rockcraft.yaml") as file:
+        rockcraft = yaml.safe_load(file)
+        name = rockcraft["name"]
+        version = rockcraft["version"]
+
+        subprocess.run([
+            "skopeo",
+            "copy",
+            f"oci-archive:{name}_{version}_amd64.rock",
+            f"docker-daemon:{name}:test",
+        ])
+
+
+def test_all_apps():
+    with open("rockcraft.yaml") as file:
+        rockcraft = yaml.safe_load(file)
+        name = rockcraft["name"]
+
+        override = {
+            "pgbackrest": "version",
+        }
+
+        apps = [
+            "/usr/bin/pgbouncer",
+            "/usr/bin/psql",
+        ]
+
+        for app in apps:
+            print(f"Running {app}...")
+            try:
+                subprocess.check_output([
+                    "docker",
+                    "run",
+                    "--entrypoint",
+                    app,
+                    f"{name}:test",
+                    override.get(app, "--help"),
+                ])
+            except subprocess.CalledProcessError as e:
+                print(e)
+                raise e
+
+
+def test_version():
+    with open("rockcraft.yaml") as file:
+        rockcraft = yaml.safe_load(file)
+        name = rockcraft["name"]
+        version = rockcraft["version"]
+        app_version = (
+            subprocess.check_output([
+                "docker",
+                "run",
+                "--entrypoint",
+                "/usr/bin/pgbouncer",
+                f"{name}:test",
+                "--version",
+            ])
+            .decode()
+            .split(" ")[2]
+        )
+        assert version == app_version
